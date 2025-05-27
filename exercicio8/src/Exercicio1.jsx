7726d64cb78284473f0fe6be0453f846f81f39f6
Rafael Ramos <rafaelhenriqueramos251@gmail.com>
2025-05-27 14:53:46 -0300

    vd
    gf

---
+++ exercicio8/src/Exercicio1.jsx
@@ -0,0 +1,15 @@
+export default function Exercicio1({numero})
+{
+    let quadrado = numero*numero;
+    let cubo = numero*numero*numero;
+
+    return (
+        <div>
+            <p>O quadrado do numero {numero} é {quadrado}</p>
+
+            <p>O cubo do numero {numero} é {cubo}</p>
+
+        </div>
+    );
+
+}
--- exercicioscomponentes/src/App.jsx
+++ exercicioscomponentes/src/App.jsx
@@ -1,18 +1,13 @@
-import Exemplo1 from "./Componentes/Exemplo1"
+import Exercicio1 from "./Exercicio1";
 
 export default function App()
 {
   return (
     <div>
-      <h1>Exercícios de Componentes</h1>
-
-      <h3>Chamada para Exemplo1</h3>
-      <Exemplo1 numero1={8} numero2={2} />
-      <Exemplo1 numero1={10} numero2={22} />
-      <Exemplo1 numero1={7.5} numero2={5.8} />
-      <Exemplo1 numero1={-58} numero2={65} />
-
+      <h1>Exercicio 1</h1>
+      <Exercicio1 numero={5}/>
+      <Exercicio1 numero={10}/>
 
     </div>
-  )
+  );
 }
