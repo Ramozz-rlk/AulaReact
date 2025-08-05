fefcbd243772a00f54fb007effc400f45b083b8d
Rafael Ramos <rafaelhenriqueramos251@gmail.com>
2025-08-05 14:43:32 -0300

    05/08

---
+++ estudorotas/src/Paginas/Home.jsx
@@ -0,0 +1,13 @@
+export default function Home()
+{
+    return (
+        <div>
+            <h1> Estudos Rotas no React</h1>
+
+<ul>
+    <li><a href="/sp">Sao Paulo</a></li>
+</ul>
+
+        </div>
+    )
+}
---
+++ estudorotas/src/Paginas/SaoPaulo.jsx
@@ -0,0 +1,20 @@
+export default function SaoPaulo()
+{
+    return(
+        <div>
+            <h1>Seja bem vindo ao Estado de sao Paulo</h1>
+
+<p>
+São Paulo é o estado mais populoso e rico do Brasil, localizado na região Sudeste. É o principal centro industrial, financeiro e cultural do país. Sua capital, São Paulo, é a maior cidade da América Latina.
+</p>
+
+<p>
+    <img src="" />
+</p>
+
+<p>
+    <a href="/">Voltar para Home</a>
+</p>
+        </div>
+    )
+}
