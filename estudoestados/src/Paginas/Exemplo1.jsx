9f22d00dc6696c4db700f7fb4702cd8f2b76714d
Rafael Ramos <rafaelhenriqueramos251@gmail.com>
2025-09-02 14:54:11 -0300

    02/09

--- estudoestados/src/Paginas/Exemplo1.jsx
+++ estudoestados/src/Paginas/Exemplo1.jsx
@@ -3,7 +3,32 @@ export default function Exemplo1() {
     <div>
       <h1>Exemplo 1</h1>
 
-      
-    </div>
+
+      <div className="conteudo">
+
+        <h3>Exemplo do useState</h3>
+
+        <p>
+          O objetivo aqui sera receber dois numeros, soma-los, e exibir o resultado.
+        </p>
+
+        <from>
+
+          <p>
+            Digite o primeiro numero <br />
+            <input type="text" />
+          </p>
+
+          <p>
+            Digite o segundo numero <br />
+            <input type="text" />
+          </p>
+
+          <p>
+            <input type="button" value="Calcular" />
+          </p>
+        </from>
+      </div>
+    </div >
   );
 }
--- estudoestados/src/Paginas/Exemplo2.jsx
+++ estudoestados/src/Paginas/Exemplo2.jsx
@@ -3,7 +3,36 @@ export default function Exemplo2() {
     <div>
       <h1>Exemplo 2</h1>
 
-      
+      <div className="conteudo">
+
+        <h3>CAlculo da media Aritimetica</h3>
+
+        <p>O objetivo sera receber o nome do aluno, suas 3 notas e calcular a media</p>
+
+ <from>
+
+          <p>
+            Nome do Aluno: <br />
+            <input type="text" />
+          </p>
+
+           <p>
+            Nota 1: <br />
+            <input type="text" />
+          </p>
+
+           <p>
+            Nota 2: <br />
+            <input type="text" />
+          </p>
+
+            <p>
+            <input type="button" value="Calcular" />
+          </p>
+
+
+ </from>
+      </div>
     </div>
   );
 }
