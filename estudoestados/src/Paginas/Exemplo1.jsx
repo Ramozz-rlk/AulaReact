2f011fd4136a7e96fcdc7ae0e6e07e013c41eb89
Rafael Ramos <rafaelhenriqueramos251@gmail.com>
2025-09-23 14:45:38 -0300

    23/9

--- estudoestados/src/Paginas/Exemplo1.jsx
+++ estudoestados/src/Paginas/Exemplo1.jsx
@@ -1,4 +1,24 @@
+import { useState } from "react";
 export default function Exemplo1() {
+  const[numero1, setNumero1] = useState(22);
+  const[numero2, setNumero2] = useState(5);
+  const[resultado, setResultado] = useState();
+
+function somar()
+{
+let n1, n2, soma;
+n1 = Number(numero1);
+n2 = Number(numero2);
+soma = n1 + n2;
+
+setResultado("a soma é: " + soma)
+}
+
+function limpar()
+  {
+
+  }
+
   return (
     <div>
       <h1>Exemplo 1</h1>
@@ -16,16 +36,26 @@ export default function Exemplo1() {
 
           <p>
             Digite o primeiro numero <br />
-            <input type="text" />
+            <input type="text" value={numero1}
+            onChange={ (e) => setNumero1(e.target.value)}/>
           </p>
 
           <p>
             Digite o segundo numero <br />
-            <input type="text" />
+            <input type="text" value={numero2}
+            onChange={ (e) => setNumero2(e.target.value)}/>
+
           </p>
 
           <p>
-            <input type="button" value="Calcular" />
+
+            <p>
+              Numero1= {numero1} <br />
+              Numero2= {numero2} <br />
+              {resultado}
+
+            </p>
+            <input type="button" value="Calcular" onClick={somar}/>
           </p>
         </from>
       </div>
--- estudoestados/src/Paginas/Exemplo2.jsx
+++ estudoestados/src/Paginas/Exemplo2.jsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
 export default function Exemplo2() {
+  const[nota1, setNota1] = useState(22);
+  const[nota2, setNota2] = useState(5);
+  const[nota3, setNota3] = useState(5);
+  const[resultado, setResultado] = useState();  
+    function media()
+{
+let n1, n2, n3, media;
+n1 = Number(nota1);
+n2 = Number(nota2);
+n3 = Number(nota3);
+
+
+media = (n1 + n2 + n3)/3;
+
+setResultado("a media é: " + media)
+}
+
+function limpar()
+  {
+
+  }
+
   return (
     <div>
       <h1>Exemplo 2</h1>
 
       <div className="conteudo">
 
-        <h3>CAlculo da media Aritimetica</h3>
+        <h3>Calculo da media Aritimetica</h3>
 
         <p>O objetivo sera receber o nome do aluno, suas 3 notas e calcular a media</p>
 
@@ -18,16 +41,33 @@ export default function Exemplo2() {
 
            <p>
             Nota 1: <br />
-            <input type="text" />
-          </p>
+            <input type="text" value={nota1}
+            onChange={ (e) => setNota1(e.target.value)}/>    
+            </p>
 
            <p>
             Nota 2: <br />
-            <input type="text" />
+            <input type="text" value={nota2}
+            onChange={ (e) => setNota2(e.target.value)}/>   
           </p>
 
+          <p>
+            Nota 3: <br />
+            <input type="text" value={nota3}
+            onChange={ (e) => setNota3(e.target.value)}/>   
+          </p>
+
+
+          <p>
+              nota1= {nota1} <br />
+              nota2= {nota2} <br />
+              nota3= {nota3} <br />
+
+              {resultado}
+
+            </p>
             <p>
-            <input type="button" value="Calcular" />
+            <input type="button" value="Calcular" onClick={media}/>
           </p>
 
 
